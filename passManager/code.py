962d040bb77445d00be8a0f83e9c0e6c377204dc
khubi sahu <vkhubisahu14@gmail.com>
2025-03-03 11:49:52 +0530

    passManager completed

--- passManager/code.py
+++ passManager/code.py
@@ -1,26 +1,35 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("enter master password: ")
-
+'''
 def write_key():
     key = Fernet.generate_key()
-    with open("key.key","wb") as key_files:
-        
+    with open("key.key","wb") as key_file:
+        key_file.write(key)
 
+write_key() '''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key() 
+fer = Fernet(key)
 
 def view():
     with open('password.txt','r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("user: ",user , ", password: ",passw)
+            print("user: ",user , ", password: ",fer.decrypt(passw.encode()).decode())
     
 def add():
     name = input("account name: ")
     pwd = input("password : ")
 
     with open('password.txt','a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" +str(fer.encrypt(pwd.encode()).decode()) + "\n")
 
     
 while True:
